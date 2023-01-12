22aab4931031ea7463ebd05df2dd7d7bb0b060e5
Bastien Guillon <bastien.guillon42@gmail.com>
2023-01-12 23:15:25 +0100

    read picture path arg

--- src/glitch.js
+++ src/glitch.js
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+const fs = require('fs');
+
+function getPic() {
+    const pic = fs.readFileSync(
+        process.argv[2] ?
+            process.argv[2] :
+            (function () { throw new Error('missing picture path arg'); })()
+    );
+    return pic;
+}
+
+const pic = getPic();
+console.log(pic);
